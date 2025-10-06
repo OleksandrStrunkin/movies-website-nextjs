5ebe751ab6ffef8d5e949208af0e5f2ddd0a3fa3
OleksandrStrunkin <oleksandr.strunkin@gmail.com>
2025-10-06 22:47:37 +0300

    feat: enhance HeroBanner styling with gradient mask and improved layout

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -16,12 +16,11 @@ export default function HeroBanner() {
     <>
       {heroMovie && (
         <section
-          className="relative h-196 bg-cover bg-center flex items-center justify-center"
+          className={`relative h-196 bg-cover flex items-center justify-center mask-alpha mask-b-from-black mask-b-from-20% mask-b-to-transparent`}
           style={{
             backgroundImage: `url(https://image.tmdb.org/t/p/original${heroMovie?.backdrop_path})`,
           }}
         >
-          <div className="absolute inset-0 bg-gradient-to-r from-black/100 via-transparent to-black/100"></div>
           <div className="relative flex flex-col gap-2 md:flex-row z-10 text-center text-white px-4 w-5xl h-2xl backdrop-blur-3xl py-8 rounded-lg">
             <Image
               src={`https://image.tmdb.org/t/p/w500${heroMovie.poster_path}`}
@@ -31,11 +30,11 @@ export default function HeroBanner() {
               className="rounded-md mx-auto"
             />
             <div>
-                <h2 className="text-4xl font-bold mb-4">{heroMovie.title}</h2>
-                <p className="mb-6">{heroMovie.overview}</p>
-                <button className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                  Watch now
-                </button>
+              <h2 className="text-4xl font-bold mb-4">{heroMovie.title}</h2>
+              <p className="mb-6">{heroMovie.overview}</p>
+              <button className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                Watch now
+              </button>
             </div>
           </div>
         </section>
